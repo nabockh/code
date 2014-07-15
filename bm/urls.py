3cb7f9baac6a78f56bc014c9c39eef8cf116638d
yhulpa <yuriy.hulpa@perfectial.com>
2014-07-15 10:47:18 +0300

    Added "Share" buttons on Details page.

--- bm/urls.py
+++ bm/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
     url(r'^create$', views.BenchmarkCreateWizardView.as_view(), name='bm_create'),
     url(r'^history$', views.BenchmarkHistoryView.as_view(), name='bm_history'),
     url(r'^welcome$', views.WelcomeView.as_view(), name='bm_welcome'),
-    url(r'^answer/(?P<slug>[-a-zA-Z0-9_]+)$', views.BaseBenchmarkAnswerView.as_view(), name='bm_answer')
+    url(r'^answer/(?P<slug>[-a-zA-Z0-9_]+)$', views.BaseBenchmarkAnswerView.as_view(), name='bm_answer'),
+    url(r'^benchmark(?P<bm_id>\d+)/details$', views.RateBenchmarkView.as_view(), name='bm_details'),
+    url(r'^benchmark(?P<bm_id>\d+)/statistic$', views.BenchmarkStatisticView.as_view(), name='bm_statistic'),
 )
