04294f3e5c66babaf35f78b2b084a3ab54759cf8
yhulpa <yuriy.hulpa@perfectial.com>
2014-07-17 12:49:18 +0300

    Fixed crash related to obsolete url.

--- bm/urls.py
+++ bm/urls.py
@@ -7,5 +7,4 @@ urlpatterns = patterns('',
     url(r'^welcome$', views.WelcomeView.as_view(), name='bm_welcome'),
     url(r'^answer/(?P<slug>[-a-zA-Z0-9_]+)$', views.BaseBenchmarkAnswerView.as_view(), name='bm_answer'),
     url(r'^benchmark(?P<bm_id>\d+)/details$', views.BenchmarkDetailView.as_view(), name='bm_details'),
-    url(r'^benchmark(?P<bm_id>\d+)/statistic$', views.BenchmarkStatisticView.as_view(), name='bm_statistic'),
 )
