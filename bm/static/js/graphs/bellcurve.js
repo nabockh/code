0e7030780fa9e0e3c6fa1cc235b7d4594d08631a
Nazar Kulynyak <nazar.kulynyak@perfectial.com>
2014-08-01 16:09:22 +0300

    bellcurve init

--- bm/static/js/graphs/bellcurve.js
+++ bm/static/js/graphs/bellcurve.js
@@ -1,18 +1,57 @@
-
 function drawBellcurveChart(chartData, divId) {
-    if (typeof google == 'undefined') {
-        throw new Exception();
-    }
-    var bellcurveChartData = google.visualization.arrayToDataTable(chartData);
+        if (typeof google == 'undefined') {
+            throw new Exception();
+        }
 
-    var bellcurveOptions = {
-        legend: 'none',
-        colors: ['#608a94'],
-        lineWidth: 3
+        var prepareData = function (avg, sd){
+            var x1 = avg - 3*sd,
+                    x2 = avg + 3*sd;
+            var d  = 6*sd/50;
+
+            var C = (sd*Math.sqrt(2*Math.PI));
+
+            var res = [];
+
+            var y = function(x){
+                var e = Math.exp(1),
+                        dr = Math.pow(x - avg, 2)/(2*Math.pow(sd,2));
+                return Math.exp(-dr) / C;
+            };
+
+            for(var x = x1; x < x2; x+=d){
+                res.push([x, y(x), y(x), 0]);
+            }
+            return res;
+        };
+
+        var bellcurveChartData = new google.visualization.DataTable();
+        bellcurveChartData.addColumn('number', 'x');
+        bellcurveChartData.addColumn('number', 'values');
+        bellcurveChartData.addColumn({id:'i0', type:'number', role:'interval'});
+        bellcurveChartData.addColumn({id:'i1', type:'number', role:'interval'});
+
+        bellcurveChartData.addRows(prepareData(chartData.avg,chartData.sd));
+
+
+        var bellcurveOptions = {
+            curveType: 'function',
+            pointSize: 0,
+            enableInteractivity: false,
+            legend: 'none',
+            colors: ['#608a94'],
+            lineWidth: 3,
+            intervals: { 'style':'area' },
+            series: [{'color': '#33626e'}],
+            chartArea: {
+                width: "80%",
+                height: "80%"
+            },
+            tooltip: {'trigger' : 'none'},
+            hAxis: {title: 'avg: ' + chartData.avg + "%"}
+        };
+
+        var bellcurveChart = new google.visualization.LineChart(document.getElementById(divId));
+
+        // main charts
+        bellcurveChart.draw(bellcurveChartData, bellcurveOptions);
     };
-
-    var bellcurveChart = new google.visualization.ScatterChart(document.getElementById(divId))
-
-    // main charts
-    bellcurveChart.draw(bellcurveChartData, bellcurveOptions);
-};
--- bm/static/js/graphs/stats.js
+++ bm/static/js/graphs/stats.js
@@ -1,53 +1,49 @@
+function Gradient(slices){
+    var startColor = { r : 96, g : 138, b : 148};
+    var endColor = { r : 32, g : 44, b : 69};
 
+    var results = {};
 
-google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawChart);
+    var deltaR = ((endColor.r + startColor.r) / slices);
+    var deltaG = ((endColor.g + startColor.g) / slices);
+    var deltaB = ((endColor.g + startColor.g) / slices);
 
+    for (var i = 0; i < slices; i++){
+        var r = Math.round(startColor.r + deltaR * i);
+        var g = Math.round(startColor.g + deltaG * i);
+        var b = Math.round(startColor.b + deltaB * i);
 
-        function Gradient(){
-            var startColor = { r : 96, g : 138, b : 148};
-            var endColor = { r : 32, g : 44, b : 69};
-            var slices = statisticRoleData.length;
+        results[i] = {'color' : 'rgb(' + r + ',' + g + ',' + b + ')'};
+    }
 
-            var results = {};
+    return results;
+}
 
-            var deltaR = ((endColor.r + startColor.r) / slices);
-            var deltaG = ((endColor.g + startColor.g) / slices);
-            var deltaB = ((endColor.g + startColor.g) / slices);
+function drawStatsChart(chartData, divId) {
+    if (typeof google == 'undefined') {
+        throw new Exception();
+    }
+    var roleChartData = google.visualization.arrayToDataTable(chartData);
+    var geoChartData = google.visualization.arrayToDataTable(chartData);
+    var countryChartData = google.visualization.arrayToDataTable(chartData);
+    var industryChartData = google.visualization.arrayToDataTable(chartData);
 
-            for (var i = 0; i < slices; i++){
-            var r = Math.round(startColor.r + deltaR * i);
-            var g = Math.round(startColor.g + deltaG * i);
-            var b = Math.round(startColor.b + deltaB * i);
-
-            results[i] = {'color' : 'rgb(' + r + ',' + g + ',' + b + ')'};
-            }
-
-            return results;
+    var statsOptions = {
+        legend: 'none',
+        pieSliceText: 'none',
+        tooltip: { text: 'percentage' },
+        slices: Gradient(),
+        chartArea: { 
+            width: "90%", 
+            height: "90%" 
         }
+    };
 
-    function drawChart() {
-        var roleChartData = google.visualization.arrayToDataTable(statisticRoleData);
-        var geoChartData = google.visualization.arrayToDataTable(statisticGeoData);
-        var countryChartData = google.visualization.arrayToDataTable(statisticCountriesData);
-        var industryChartData = google.visualization.arrayToDataTable(statisticIndustriesData);
 
-        var statsOptions = {
-            legend: 'none',
-            pieSliceText: 'none',
-            tooltip: { text: 'percentage' },
-            slices: Gradient(),
-            chartArea: { 
-                width: "90%", 
-                height: "90%" 
-            }
-        };
-    
-
-    var roleChart = new google.visualization.PieChart(document.getElementById('roleChart'));
-    var geoChart = new google.visualization.PieChart(document.getElementById('geoChart'));
-    var countryChart = new google.visualization.PieChart(document.getElementById('countryChart'));
-    var industryChart = new google.visualization.PieChart(document.getElementById('industryChart'));
+    var roleChart = new google.visualization.PieChart(document.getElementById('divId'));
+    var geoChart = new google.visualization.PieChart(document.getElementById('divId'));
+    var countryChart = new google.visualization.PieChart(document.getElementById('divId'));
+    var industryChart = new google.visualization.PieChart(document.getElementById('divId'));
 
     
     // stat charts
