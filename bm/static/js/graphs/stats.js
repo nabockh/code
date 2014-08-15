1132c1824c251656771f9ec77b7f170575d106d8
Nazar Kulynyak <nazar.kulynyak@perfectial.com>
2014-08-15 17:58:42 +0300

    @media fix, Stat charts color fix

--- bm/static/js/graphs/stats.js
+++ bm/static/js/graphs/stats.js
@@ -1,12 +1,12 @@
 function statsGradient(slices){
-    var startColor = { r : 96, g : 138, b : 148};
-    var endColor = { r : 32, g : 44, b : 69};
+    var startColor = { r : 32, g : 44, b : 69};
+    var endColor = { r : 96, g : 138, b : 148};
 
     var results = {};
 
-    var deltaR = ((endColor.r + startColor.r) / slices);
-    var deltaG = ((endColor.g + startColor.g) / slices);
-    var deltaB = ((endColor.g + startColor.g) / slices);
+    var deltaR = ((endColor.r - startColor.r) / slices);
+    var deltaG = ((endColor.g - startColor.g) / slices);
+    var deltaB = ((endColor.g - startColor.g) / slices);
 
     for (var i = 0; i < slices; i++){
         var r = Math.round(startColor.r + deltaR * i);
