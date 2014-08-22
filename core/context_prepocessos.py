15289efdd160eea3758eeee043784aae2898188d
Petro Zdeb <petro.zdeb@perfectial.com>
2014-08-22 17:55:09 +0300

    Fixed BDDE-295: changed sharing buttons from using addthis library to static URLs. We have more control over URL params in such way

--- core/context_prepocessos.py
+++ core/context_prepocessos.py
@@ -1,5 +1,7 @@
 from core.forms import ContactForm
+from django.contrib.sites.models import get_current_site
 
 
 def contact_form(request):
-    return {'contact_form': ContactForm(request=request)}
+    return {'contact_form': ContactForm(request=request),
+            'absolute_url': request.build_absolute_uri()}
