c56065132750a1516d2cc90baf8f29467b719f17
Volodymyr Trotsyshyn <devova@gmail.com>
2014-09-11 13:20:32 +0300

    cms popups page /cms-popups

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from core import views
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^beta$', views.BetaView.as_view(), name='beta'),
+    url(r'^cms-popups$', views.CMSPopupsView.as_view(), name='cms_popup'),
     # url(r'^terms$', views.TermsAndConditionsView.as_view(), name='terms_conditions'),
-
 )
