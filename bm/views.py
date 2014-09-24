0c4f9722a4bdacf5c255bf526fb490f318f36603
yhulpa <yuriy.hulpa@perfectial.com>
2014-09-24 11:35:21 +0300

    Removed additional_comments from email_preview

--- bm/views.py
+++ bm/views.py
@@ -198,7 +198,6 @@ class BenchmarkCreateWizardView(CookieWizardView):
             else:
                 benchmark.industry = step3.cleaned_data['industry']
             benchmark.min_numbers_of_responses = step3.cleaned_data['minimum_number_of_answers']
-            benchmark.overview = step3.cleaned_data['additional_comments']
             if preview:
                 self.benchmark = benchmark
                 return self.benchmark
