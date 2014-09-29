680f855d5e9d5cb26f5ac36bb2fedbf4ea51447d
Petro Zdeb <petro.zdeb@perfectial.com>
2014-09-29 17:06:22 +0300

    Moved sensitive configuration into db in encrypted view

--- app/celery.py
+++ app/celery.py
@@ -17,6 +17,12 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
+from core.models import SystemKey
+smtp_keys = SystemKey.get_keys('SendGrid SMTP')
+settings.EMAIL_HOST_USER = smtp_keys['EMAIL_HOST_USER']
+settings.EMAIL_HOST_PASSWORD= smtp_keys['EMAIL_HOST_PASSWORD']
+
+
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
