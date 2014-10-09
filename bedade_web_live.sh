1338c2151ca9d5e6ea6b42dbda65a9301307d8b3
Petro Zdeb <petro.zdeb@perfectial.com>
2014-10-09 12:40:32 +0300

    Added NewRelic configuration

--- bedade_web_live.sh
+++ bedade_web_live.sh
@@ -8,6 +8,7 @@ GROUP=webapps                                     # the group to run as
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
 DJANGO_SETTINGS_MODULE=app.settings             # which settings file should Django use
 DJANGO_WSGI_MODULE=app.wsgi                     # WSGI module name
+NEW_RELIC_CONFIG_FILE=newrelic.ini
  
 echo "Starting $NAME as `whoami`"
  
@@ -23,7 +24,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
  
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec newrelic-admin run-program gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
