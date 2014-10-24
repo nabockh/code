55dce89152cdebf16300ab367b3068d890c2eb29
yhulpa <yuriy.hulpa@perfectial.com>
2014-10-24 11:49:00 +0300

    Fixed BDDE-909

--- social/signals.py
+++ social/signals.py
@@ -4,7 +4,7 @@ from social.models import Invite
 from django.db.models.signals import post_save
 from django.core.mail import send_mail
 from django.template import loader, Context
-
+from django.contrib.sites.models import Site
 
 first_time_user_login = Signal(providing_args=['user', ])
 invitee_approved = Signal(providing_args=['approved_invites', ])
@@ -19,6 +19,7 @@ def notify_invitee(instance, **kwargs):
     if instance.allowed and kwargs['update_fields']:
         template = loader.get_template('alerts/beta_notification.html')
         context = Context({
-            'email': instance.email
+            'email': instance.email,
+            'site_link': Site.objects.get_current()
         })
         send_mail('Your invite was approved', template.render(context), None, [instance.email])
