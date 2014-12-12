96bc0360d0a67497093bc8396c7485d8a325b64b
yhulpa <yuriy.hulpa@perfectial.com>
2014-12-12 20:45:51 +0200

    Fixed Area Chart for Numeric in Excel.

--- bm/views.py
+++ bm/views.py
@@ -1009,10 +1009,10 @@ class ExcelDownloadView(BenchmarkDetailView):
                 contributor_worksheet.write_row('B%s' % idx, [val, ])
             chart_area.add_series({
                 'name':         benchmark.name,
-                'categories': "='Contributor Stats'!A7:A{0}".format(len(area_data) + 7),
-                'values': "='Contributor Stats'!B7:B{0}".format(len(area_data) + 7),
+                'categories': "='Contributor Stats'!A7:A{0}".format(len(area_data[1:]) + 6),
+                'values': "='Contributor Stats'!B7:B{0}".format(len(area_data[1:]) + 6),
             })
-            # chart_bell_curve.set_y_axis({'num_format': ''})
+            chart_bell_curve.set_y_axis({'num_format': ''})
             contributor_worksheet.insert_chart('F6', chart_bell_curve)
             contributor_worksheet.insert_chart('F23', chart_area)
             # internal_worksheet.protect()
